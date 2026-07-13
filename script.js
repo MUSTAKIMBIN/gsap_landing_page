c9b0dbc72abc674aba849b525f5a7a14518c7603
MUSTAKIMBIN <mustakimbin24@gmail.com>
2026-07-13 19:14:27 +0600

    added first section basics andimation

---
+++ script.js
@@ -0,0 +1,17 @@
+let tl = gsap.timeline();
+tl.from("#red", {
+  x: 1000,
+  scale: 0,
+  duration: 1.5,
+  delay: 0.5,
+});
+tl.from("#blue", {
+  x: 1000,
+  scale: 0,
+  duration: 1.5,
+});
+tl.from("#green", {
+  x: 1000,
+  scale: 0,
+  duration: 1.5,
+});
